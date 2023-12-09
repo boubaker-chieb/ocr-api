c9ad33366a5d5a63d4fdc253939c270d87003a8a
Boubaker Chieb <boubaker.ch@outlook.fr>
2023-12-09 16:23:21 +0100

    fix

--- src/services/ocr.service.ts
+++ src/services/ocr.service.ts
@@ -1,13 +1,12 @@
 import { OcrDto } from "src/dtos";
 import { toOcrDTo } from "src/dtos/ocr.dto";
-import { RecognizeResult } from "tesseract.js";
 import Tesseract from "tesseract.js";
 
 export class OcrService {
   public async scanImage(file: Express.Multer.File, language = 'eng'): Promise<OcrDto> {
-    const worker = await Tesseract.createWorker(language);
-    const ret: RecognizeResult = await worker.recognize(file.buffer);
-    await worker.terminate();
+   Tesseract.createWorker(language);
+   console.log(language)
+    const ret: any = await Tesseract.recognize(file.buffer);
     return toOcrDTo(ret);
   }
 }
