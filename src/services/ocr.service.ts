db0ee5cf0ddce8e75ff39af37ed10f30f7d13f87
Boubaker Chieb <boubaker.ch@outlook.fr>
2023-12-24 02:09:31 +0100

    test

--- src/services/ocr.service.ts
+++ src/services/ocr.service.ts
@@ -1,14 +1,14 @@
 import { OcrDto } from "src/dtos";
 import { toOcrDTo } from "src/dtos/ocr.dto";
-import { OEM, RecognizeResult, createWorker } from "tesseract.js";
+import Tesseract from "tesseract.js";
 
 export class OcrService {
   public async scanImage(file: Express.Multer.File, language = 'eng'): Promise<OcrDto> {
-    const worker = await createWorker(language, 1, {
+    const worker = await Tesseract.createWorker(language, 1, {
       langPath: 'https://tessdata.projectnaptha.com/4.0.0',
       corePath: 'https://cdn.jsdelivr.net/npm/tesseract.js-core@v5.0.0',
     });
-    const ret: RecognizeResult = await worker.recognize(file.buffer, {});
+    const ret: Tesseract.RecognizeResult = await worker.recognize(file.buffer, {});
     await worker.terminate();
     return toOcrDTo(ret);
   }
